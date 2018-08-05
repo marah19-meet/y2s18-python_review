edfc0f4621a8912f56390fbcc600695d8b24acd2
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 16:33:47 +0300

    l

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,13 @@
-# Write your solution for 1.2 here!
+a=0
+for i in range (101):
+    if i % 2 == 0:
+        a=a+i
+print(a)
+
+a=0
+for i in range (1001):
+    if i<1001 and i%6=2 and i**%5=3 :
+print (i)
+
+
+        
--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,4 @@
-# Write your solution for 1.1 here!
+a=0
+for i in range(100) :
+    a=a+1
+print (a)
