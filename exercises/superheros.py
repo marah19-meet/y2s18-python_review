9b7f3893146b3c13a9e78b83f37d8e3750727cce
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 23:01:40 +0300

    g

--- exercises/superheros.py
+++ exercises/superheros.py
@@ -1,1 +1,13 @@
-# Write your solutions for 1.5 here!
+class Superheros():
+    def __init__(self,name,superpower,strength):
+        self.name=name
+        self.superpower=superpower
+        self.strength=strength
+        print("my name is"+self.name + "my strength is" + str(self.strength))
+    def save_civillian(self):
+        print(self.name,self.strength)
+
+
+
+
+superman = Superheros("superman","flying",100)
